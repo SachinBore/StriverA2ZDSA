5b412512b7b994358b9ef23d8dd5a6ea9911a8bc
Sachin <58112222+SachinBore@users.noreply.github.com>
2023-06-30 11:15:28 +0530

    Updated pattern_7.cpp

--- Patterns/pattern_7.cpp
+++ Patterns/pattern_7.cpp
@@ -10,7 +10,7 @@ int main()
     for(int i=0; i<n; i++) {
         
         for(int j=i; j<n-1; j++) {
-            cout<<"-";
+            cout<<" ";
         }
         
         for(int j=0; j<2*i+1; j++) {
